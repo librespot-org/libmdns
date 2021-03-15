16133e85be6b3c16a44b36ccc54c219e238ed83c
johannesd3 <johannesd@outlook.com>
2021-03-15 01:20:35 +0100

    Use thiserror instead of quick-error

--- src/dns_parser/error.rs
+++ src/dns_parser/error.rs
@@ -1,46 +1,29 @@
-use quick_error::quick_error;
+use thiserror::Error;
 
-quick_error! {
-    /// Error parsing DNS packet
-    #[derive(Debug)]
-    #[allow(dead_code)]
-    pub enum Error {
-        HeaderTooShort {
-            description("packet is smaller than header size")
-        }
-        UnexpectedEOF {
-            description("packet is has incomplete data")
-        }
-        WrongRdataLength {
-            description("wrong (too short or too long) size of RDATA")
-        }
-        ReservedBitsAreNonZero {
-            description("packet has non-zero reserved bits")
-        }
-        UnknownLabelFormat {
-            description("label in domain name has unknown label format")
-        }
-        InvalidQueryType(code: u16) {
-            description("query type code is invalid")
-            display("query type {} is invalid", code)
-        }
-        InvalidQueryClass(code: u16) {
-            description("query class code is invalid")
-            display("query class {} is invalid", code)
-        }
-        InvalidType(code: u16) {
-            description("type code is invalid")
-            display("type {} is invalid", code)
-        }
-        InvalidClass(code: u16) {
-            description("class code is invalid")
-            display("class {} is invalid", code)
-        }
-        LabelIsNotAscii {
-            description("invalid characters encountered while reading label")
-        }
-        WrongState {
-            description("parser is in the wrong state")
-        }
-    }
+/// Error parsing DNS packet
+#[derive(Debug, Error)]
+#[allow(dead_code)]
+pub enum Error {
+    #[error("packet is smaller than header size")]
+    HeaderTooShort,
+    #[error("packet is has incomplete data")]
+    UnexpectedEOF,
+    #[error("wrong (too short or too long) size of RDATA")]
+    WrongRdataLength,
+    #[error("packet has non-zero reserved bits")]
+    ReservedBitsAreNonZero,
+    #[error("label in domain name has unknown label format")]
+    UnknownLabelFormat,
+    #[error("query type {0} is invalid")]
+    InvalidQueryType(u16),
+    #[error("query class {0} is invalid")]
+    InvalidQueryClass(u16),
+    #[error("type {0} is invalid")]
+    InvalidType(u16),
+    #[error("class {0} is invalid")]
+    InvalidClass(u16),
+    #[error("invalid characters encountered while reading label")]
+    LabelIsNotAscii,
+    #[error("parser is in the wrong state")]
+    WrongState,
 }
