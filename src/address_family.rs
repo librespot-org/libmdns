745acd8fd93bc088933a3ce4032f316f6c110570
johannesd3 <johannesd@outlook.com>
2021-03-15 01:22:46 +0100

    Refactor AddressFamily trait and bump socket2

--- src/address_family.rs
+++ src/address_family.rs
@@ -4,60 +4,60 @@ use std::io;
 use std::net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr, UdpSocket};
 
 pub enum Inet {}
+
 pub enum Inet6 {}
 
 pub trait AddressFamily {
-    fn bind() -> io::Result<UdpSocket> {
-        let addr = SockAddr::from(SocketAddr::new(Self::any_addr(), MDNS_PORT));
-        let socket = Self::socket_builder()?;
-        socket.set_reuse_address(true)?;
-        socket.set_nonblocking(true)?;
-        #[cfg(not(windows))]
-        let _ = socket.set_reuse_port(true)?;
-        socket.bind(&addr)?;
-        Self::join_multicast(&socket)?;
-        Ok(socket.into_udp_socket())
+    type Addr: Into<IpAddr>;
+
+    const ANY_ADDR: Self::Addr;
+    const MDNS_GROUP: Self::Addr;
+
+    const DOMAIN: Domain;
+
+    fn join_multicast(socket: &Socket, multiaddr: &Self::Addr) -> io::Result<()>;
+
+    fn udp_socket() -> io::Result<Socket> {
+        Socket::new(Self::DOMAIN, Type::DGRAM, Some(Protocol::UDP))
     }
 
-    fn socket_builder() -> io::Result<Socket>;
-    fn any_addr() -> IpAddr;
-    fn mdns_group() -> IpAddr;
-    fn join_multicast(socket: &Socket) -> io::Result<()>;
-    fn v6() -> bool;
+    fn bind() -> io::Result<UdpSocket> {
+        let addr: SockAddr = SocketAddr::new(Self::ANY_ADDR.into(), MDNS_PORT).into();
+        let socket = Self::udp_socket()?;
+        socket.set_reuse_address(true)?;
+        socket.set_nonblocking(true)?;
+
+        #[cfg(not(windows))]
+        socket.set_reuse_port(true)?;
+
+        socket.bind(&addr)?;
+        Self::join_multicast(&socket, &Self::MDNS_GROUP)?;
+        Ok(socket.into())
+    }
 }
 
 impl AddressFamily for Inet {
-    fn socket_builder() -> io::Result<Socket> {
-        Socket::new(Domain::ipv4(), Type::dgram(), Some(Protocol::udp()))
-    }
-    fn any_addr() -> IpAddr {
-        IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0))
-    }
-    fn mdns_group() -> IpAddr {
-        IpAddr::V4(Ipv4Addr::new(224, 0, 0, 251))
-    }
-    fn join_multicast(socket: &Socket) -> io::Result<()> {
-        socket.join_multicast_v4(&Ipv4Addr::new(224, 0, 0, 251), &Ipv4Addr::new(0, 0, 0, 0))
-    }
-    fn v6() -> bool {
-        false
+    type Addr = Ipv4Addr;
+
+    const ANY_ADDR: Self::Addr = Ipv4Addr::UNSPECIFIED;
+    const MDNS_GROUP: Self::Addr = Ipv4Addr::new(224, 0, 0, 251);
+
+    const DOMAIN: Domain = Domain::IPV4;
+
+    fn join_multicast(socket: &Socket, multiaddr: &Self::Addr) -> io::Result<()> {
+        socket.join_multicast_v4(multiaddr, &Ipv4Addr::UNSPECIFIED)
     }
 }
 
 impl AddressFamily for Inet6 {
-    fn socket_builder() -> io::Result<Socket> {
-        Socket::new(Domain::ipv6(), Type::dgram(), Some(Protocol::udp()))
-    }
-    fn any_addr() -> IpAddr {
-        IpAddr::V6(Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 0))
-    }
-    fn mdns_group() -> IpAddr {
-        IpAddr::V6(Ipv6Addr::new(0xff02, 0, 0, 0, 0, 0, 0, 0xfb))
-    }
-    fn join_multicast(socket: &Socket) -> io::Result<()> {
-        socket.join_multicast_v6(&Ipv6Addr::new(0xff02, 0, 0, 0, 0, 0, 0, 0xfb), 0)
-    }
-    fn v6() -> bool {
-        true
+    type Addr = Ipv6Addr;
+
+    const ANY_ADDR: Self::Addr = Ipv6Addr::UNSPECIFIED;
+    const MDNS_GROUP: Self::Addr = Ipv6Addr::new(0xff02, 0, 0, 0, 0, 0, 0, 0xfb);
+
+    const DOMAIN: Domain = Domain::IPV6;
+
+    fn join_multicast(socket: &Socket, multiaddr: &Self::Addr) -> io::Result<()> {
+        socket.join_multicast_v6(multiaddr, 0)
     }
 }
